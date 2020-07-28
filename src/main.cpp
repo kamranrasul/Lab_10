284e636fabf495e837d72aea0801a27e343b7410
kamranrasul <kamran.rasul@gmail.com>
2020-07-28 11:58:49 -0600

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,44 @@
 #include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
+ 
+// Pin Array definition
+int8_t ledPin[8] = { 2, 3, 4, 5, 6, 7, 8, 9 };
+ 
+// pattern
+int8_t count = 0;
+ 
+// flag for up and down streams
+bool flag = false;
+ 
+void setup()
+{
+  // setting up serial
+  Serial.begin(115200);
+ 
+  // setting Pins for Output
+  for (int i = 0; i < 8; i++)
+  {
+    // setting Pin for OUTPUT
+    pinMode(ledPin[i], OUTPUT);
+ 
+    // setting all Pins to LOW
+    digitalWrite(ledPin[i], LOW);
+  }
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+ 
+void loop()
+{
+  // when all LED up, start counting down
+  digitalWrite(ledPin[flag ? count-- : count++], flag ? LOW : HIGH);
+ 
+  // switching from down to up count
+  flag = count == -1 ? count = 0 : flag;
+ 
+  // switching from up to down count
+  flag = count == 8 ? count = 7 : flag;
+ 
+  // writing the pin to serial
+  Serial.println(count);
+ 
+  // wait for 1 second
+  delay(500);
 }
